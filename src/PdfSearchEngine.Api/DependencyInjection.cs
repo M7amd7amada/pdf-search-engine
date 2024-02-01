16c420f342c5e16c31b45abf799b81cbed9cbec4
Mohammed Hamada <m7amd7amada@gmail.com>
2024-02-01 09:38:47 +0200

    Setup The Project

---
+++ src/PdfSearchEngine.Api/DependencyInjection.cs
@@ -0,0 +1,13 @@
+namespace PdfSearchEngine.Api;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPresentation(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        services.AddControllers();
+
+        return services;
+    }
+}
